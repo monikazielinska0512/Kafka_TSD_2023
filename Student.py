4f967711f37413c375fe61c13baab2a308756b2f
monikazielinska0512 <monika.zielinska0512@gmail.com>
2023-05-16 16:11:25 +0200

    remove Student class

--- Student.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
-
-    def calculate_gpa(self):
-        total_marks = sum(self.marks)
-        gpa = total_marks / len(self.marks)
-        return gpa
